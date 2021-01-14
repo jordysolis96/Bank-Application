a9b501ae662c125e25b4e57f4441b110e9c026b0
jordy muniz <jordy.muniz96@gmail.com>
2021-01-14 11:48:59 -0600

    minor menu fixes

--- src/Bank.java
+++ src/Bank.java
@@ -5,13 +5,14 @@ public class Bank {
 
     public static boolean bankMenu() throws IOException {
         Scanner choice = new Scanner(System.in);
-        System.out.println(1. + "." + "Check balance");
-        System.out.println(4. + "." + "Withdraw");
-        System.out.println(2. + "." + "Deposit");
-        System.out.println(3. + "." + "Transfer");
-        System.out.println(5. + "." + "Quit");
+        System.out.println(1 + "." + " " + "Check balance");
+        System.out.println(4 + "." + " " + "Withdraw");
+        System.out.println(2 + "." + " " + "Deposit");
+        System.out.println(3 + "." + " " + "Transfer");
+        System.out.println(5 + "." + " " + "Quit");
         System.out.println("Select an option by entering the corresponding number.");
         int Choice = choice.nextInt();
         return bankMenu();
     }
+
 }
