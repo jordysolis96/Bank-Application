0759c38fa6fc54a398a485f07554d19516f03cb9
jordy muniz <jordy.muniz96@gmail.com>
2021-01-16 20:21:45 -0600

    added menu to version 2

--- src/Version2/BankAppv2.java
+++ src/Version2/BankAppv2.java
@@ -1,12 +1,19 @@
 package Version2;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BankAppv2 {
     public static void main(String[] args) {
-       Random rand = new Random();
-       int upperbound = 100;
-       int balance = rand.nextInt(upperbound);
-        System.out.println(balance);
+        Random rand = new Random();
+        int upperbound = 100;
+        int balance = rand.nextInt(upperbound);
+        Scanner choice = new Scanner(System.in);
+        System.out.println(1 + "." + " " + "Check balance");
+        System.out.println(2 + "." + " " + "Version1.Withdraw");
+        System.out.println(3 + "." + " " + "Deposit");
+        System.out.println(4 + "." + " " + "Transfer");
+        System.out.println(5 + "." + " " + "Quit");
+        System.out.println("Select an option by entering the corresponding number.");
+        int Choice = choice.nextInt();
     }
-
 }
